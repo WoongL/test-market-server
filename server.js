0646d0a5225cfcc261ff6eafa0e2fcf307e3667b
이웅 <dldnd2000kr@naver.com>
2021-06-25 20:11:30 +0900

    soldout 관련 api추가

--- server.js
+++ server.js
@@ -21,11 +21,34 @@ app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
+app.get("/banners", (req, res) => {
+  models.Banner.findAll({
+    limit: 2,
+  })
+    .then((result) => {
+      res.send({
+        banners: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러발생");
+    });
+});
+
 app.get("/products", (req, res) => {
   models.product
     .findAll({
       order: [["createdAt", "DESC"]],
-      attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl"],
+      attributes: [
+        "id",
+        "name",
+        "price",
+        "createdAt",
+        "seller",
+        "imageUrl",
+        "soldout",
+      ],
     })
     .then((result) => {
       console.log("products:", result);
@@ -91,6 +114,30 @@ app.post("/image", upload.single("image"), (req, res) => {
   res.send({
     imageUrl: file.path,
   });
+});
+
+app.post("/purchase/:id", (req, res) => {
+  const { id } = req.params;
+  models.product
+    .update(
+      {
+        soldout: 1,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    )
+    .then((result) => {
+      res.send({
+        result: true,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("에러발생");
+    });
 });
 
 app.listen(port, () => {
